d803cf915cb3733221a399bb5dcd09630b025914
Chatmalow <maxime.pie.mail@gmail.com>
2022-05-02 13:50:16 +0200

    fix(Task): Fix cron update

--- crons/tasks.js
+++ crons/tasks.js
@@ -7,8 +7,10 @@ async function updateTasks (request, response) {
   tasks.map((task) => {
     const {lastTimeDone, repetitionDelay} = task;
     const nextDutyTime = moment(lastTimeDone).add(repetitionDelay, 'd');
-    if (moment().isSameOrAfter(nextDutyTime)) {
-      task.isDone = false;
+    if (moment().isSameOrAfter(nextDutyTime, "h")) {
+      console.log("Updated");
+      console.log(task);
+      task.isOK = false;
       task.save();
       io.emit("tasks list updated")
     }
